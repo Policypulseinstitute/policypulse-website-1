c56e45ac764eafbccb2153ebe650dbe4f38f877d
samstouli <samstouli@gmail.com>
2023-09-15 01:47:08 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,16 +18,7 @@ function Navbar() {
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
-              <Link
-                to='/team'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                What We Do
-              </Link>
-            </li>            
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>          
             <li className='nav-item'>
               <Link
                 to='/team'

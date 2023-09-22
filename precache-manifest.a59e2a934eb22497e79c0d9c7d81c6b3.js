1c86563258204dabf3cd457d5f69be7a6702d052
Diego Lara <diegolaradeandres@gmail.com>
2023-09-22 17:43:02 +0100

    Updates

---
+++ precache-manifest.a59e2a934eb22497e79c0d9c7d81c6b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3ac28cb653dc2a2be52",
+    "url": "/static/css/main.b5d88b7e.chunk.css"
+  },
+  {
+    "revision": "e3ac28cb653dc2a2be52",
+    "url": "/static/js/main.e3ac28cb.chunk.js"
+  },
+  {
+    "revision": "9ad70fa4f499183328c3",
+    "url": "/static/js/1.9ad70fa4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cfbc8562a73b2786c6d6b3b67361a75a",
+    "url": "/index.html"
+  }
+];

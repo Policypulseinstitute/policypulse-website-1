eb2c0ef24a0a6bd512420730f0b1af7a12b3549c
samstouli <samstouli@gmail.com>
2023-10-29 14:28:53 -0400

    trying to deploy

---
+++ build/precache-manifest.d8ad59c735a4d07f6cbe35de7a51706b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "becb0e4db45191440f69",
+    "url": "/static/css/main.cc031c5d.chunk.css"
+  },
+  {
+    "revision": "becb0e4db45191440f69",
+    "url": "/static/js/main.becb0e4d.chunk.js"
+  },
+  {
+    "revision": "977be98cd7f584c133df",
+    "url": "/static/js/1.977be98c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ccd10b143529763bfd98d8f4726c5af",
+    "url": "/static/media/bkg-owl.2ccd10b1.png"
+  },
+  {
+    "revision": "f4e3968cc8286ce3f273d9fdeeba22b5",
+    "url": "/static/media/bkg-pillars.f4e3968c.png"
+  },
+  {
+    "revision": "be64029d0f78a578fedee22ef5365fa6",
+    "url": "/static/media/bkg-pillars3.be64029d.png"
+  },
+  {
+    "revision": "e28526c82368817c55ce7a3718f5c5dd",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,11 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
+<<<<<<< Updated upstream
   "/precache-manifest.7d3ea6e3d9c9883a20d5d18320885fe5.js"
+=======
+  "/precache-manifest.d8ad59c735a4d07f6cbe35de7a51706b.js"
+>>>>>>> Stashed changes
 );
 
 workbox.clientsClaim();

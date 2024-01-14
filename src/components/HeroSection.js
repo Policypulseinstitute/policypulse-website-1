c8acee3f4748f3ccec15a95385c0e496706cc0fe
samstouli <samstouli@gmail.com>
2024-01-14 23:18:58 +0000

    More mobile adjustments

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,14 +5,14 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/home1.mp4' autoPlay loop muted />
+      <video src='/videos/home1.mp4' autoPlay loop muted playsInline />
       <h1>
       Knowledge and Data Analytics<br />
       <span className="second-line">for Policy</span>
       </h1>
       <div className='hero-inner'>
         <h2>
-        Blending academic insight and policy expertise to provide innovative and evidence-based solutions.
+        Blending academic insight and policy expertise to equip leaders and organisations with innovative, evidence-based solutions.
         </h2>
       </div>
     </div>
